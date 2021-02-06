147826a35c7c0788110efbba0ff2f11bdc2592b0
Providence Salumu <Providence.Salumu@smunix.com>
2021-02-06 17:27:03 -0500

    flake, add defaultPackage

--- flake.nix
+++ flake.nix
@@ -11,51 +11,14 @@
       with (import nixpkgs { inherit system; });
       with haskellPackages;
       with haskell.lib;
-      let fixConfigFlags = drv: appendConfigureFlags drv [
-            "--ghc-option=-optl=-L${libGL}/lib"
-            "--ghc-option=-optl=-L${libGLU}/lib"
-            # "--ghc-option=-optl=-L${mesa}/lib"
-            "--ghc-options=-optl=-L${freeglut}/lib"
-            "--ghc-options=-optl=-lglut"
-            "--ghc-options=-optl=-lGL"
-            "--ghc-options=-optl=-lGLU"
-          ];
-          OpenGLRaw = fixConfigFlags (overrideCabal (callHackage "OpenGLRaw" "3.3.4.0" {}) (_: {
-            librarySystemDepends = [ libGL libGLU ];
-          }));
-          GLURaw = fixConfigFlags (overrideCabal (callHackage "GLURaw" "2.0.0.4" {
-            inherit
-              OpenGLRaw
-            ;
-          }) (_: {
-            librarySystemDepends = [ libGL libGLU ];
-          }));
-          OpenGL = fixConfigFlags (overrideCabal (callHackage "OpenGL" "3.0.3.0" {
-            inherit
-              OpenGLRaw
-              GLURaw
-            ;
-          }) (_: {
-            librarySystemDepends = [ libGL libGLU ];
-          }));
-          gloss = fixConfigFlags (overrideCabal (callHackage "gloss" "1.13.2.1" {
-            inherit
-              OpenGL
-            ;
-          }) (_: {
-            librarySystemDepends = [ libGL libGLU ];
-          }));
-      in {
-        packages = flattenTree rec {
+      rec {
+        packages = flattenTree {
           svaders =
-            fixConfigFlags (overrideCabal (callCabal2nix "svaders" ./. {
-              # inherit
-              #   gloss
-              # ;
-            }) (_: {
+            fixConfigFlags (overrideCabal (callCabal2nix "svaders" ./. {}) (_: {
               librarySystemDepends = [ libGL libGLU ];
               doHaddock = false;
             }));
         };
+        defaultPackage = packages.svaders;
       });
 }
